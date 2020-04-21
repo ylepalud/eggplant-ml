6d1ab5cb265392a49113af2809eec1cac03580e2
Le palud Yves <ylepalud@oui.sncf>
2020-04-21 14:16:37 +0200

    Modification .env file

--- README.md
+++ README.md
@@ -2,30 +2,12 @@
 
 This project need a .env file with this shape in order to work
 
-#### Database configuration
-
+# Database configuration
 DB_URL=
-
 DB_PORT=
 
-DATABASE_NAME=
-
-CLASSIFIER_COLLECTION=
-
-DATASET_COLLECTION=
-
-TEST_COLLECTION=
-
-#### RabbitMq configuration
-
+# RabbitMq configuration
 RABBIT_MQ_HOST=
-
 RABBIT_MQ_PORT=
-
 RABBIT_MQ_SERVER=
 
-SCENARIO_QUEUE=
-
-PREDICTION_QUEUE=
-
-TRAINING_QUEUE=
--- db/__init__.py
+++ db/__init__.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 DB_URL = os.getenv("DB_URL")
 DB_PORT = int(os.getenv("DB_PORT"))
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-CLASSIFIER_COLLECTION = os.getenv("CLASSIFIER_COLLECTION")
-DATASET_COLLECTION = os.getenv("DATASET_COLLECTION")
-TEST_COLLECTION = os.getenv("TEST_COLLECTION")
+
+DATABASE_NAME = "test"
+CLASSIFIER_COLLECTION = "classifier"
+DATASET_COLLECTION = "dataset"
+TEST_COLLECTION = "test_collection"
 
 
 def _create_collection_instance(db_url: str, db_port: int, db_name: str, collection_name: str):
