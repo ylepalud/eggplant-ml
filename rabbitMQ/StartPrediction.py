c7e82e8680e3483c9bebf74094e03b04b79e0be1
Le palud Yves <ylepalud@oui.sncf>
2020-05-14 14:37:55 +0200

    Add error handling in queues

--- rabbitMQ/StartPrediction.py
+++ rabbitMQ/StartPrediction.py
@@ -1,12 +1,17 @@
 from service.PredictionService import PredictionService
 from rabbitMQ.ScenarioConsumer import ScenarioConsumer
+from pika.exceptions import AMQPError
 
 
 def StartPrediction():
-    prediction_service = PredictionService()
-    scenario_consumer = ScenarioConsumer(prediction_service.make_a_prediction)
-    scenario_consumer.start()
-    scenario_consumer.close()
+    try:
+        prediction_service = PredictionService()
+        scenario_consumer = ScenarioConsumer(prediction_service.make_a_prediction)
+        scenario_consumer.start()
+    except AMQPError:
+        print("Prediction queue Error, restarting queue")
+        scenario_consumer.close()
+        StartPrediction()
 
 
 if __name__ == '__main__':
--- rabbitMQ/StartTraining.py
+++ rabbitMQ/StartTraining.py
@@ -1,12 +1,17 @@
 from service.TrainingService import TrainingService
 from rabbitMQ.TrainingConsumer import TrainingConsumer
+from pika.exceptions import AMQPError
 
 
 def StartTraining():
-    training_service = TrainingService()
-    training_consumer = TrainingConsumer(training_service.generate_new_model)
-    training_consumer.start()
-    training_consumer.close()
+    try:
+        training_service = TrainingService()
+        training_consumer = TrainingConsumer(training_service.generate_new_model)
+        training_consumer.start()
+    except AMQPError:
+        print("Training queue Error, restarting queue")
+        training_consumer.close()
+        StartTraining()
 
 
 if __name__ == '__main__':
