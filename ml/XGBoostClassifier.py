58f42bb79bea239cdf57074b1ee5cb7d496ffefa
Le palud Yves <ylepalud@oui.sncf>
2020-05-25 09:55:01 +0200

    Improve model training strategy

--- ml/XGBoostClassifier.py
+++ ml/XGBoostClassifier.py
@@ -2,8 +2,11 @@ from ml.ScenarioFormatter import ScenarioFormatter
 from model.TrainingScenario import TrainingScenario
 from model.PredictionScenario import PredictionScenario
 import numpy as np
-import xgboost as xgb
+from xgboost import XGBClassifier
 import random
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, KFold
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
 
 
 class XGBoostClassifier:
@@ -15,19 +18,41 @@ class XGBoostClassifier:
         
     def train(self, dataset: [TrainingScenario]) -> float:
         (train_labels, train_rows), (test_labels, test_rows) = self._prepare_data_for_training(dataset)
-        self._trained_model = xgb.XGBClassifier(
-            learning_rate=0.8,
-            n_estimators=140,
-            max_depth=9,
-            min_child_weight=1,
-            gamma=0.3,
-            subsample=0.8,
-            colsample_bytree=0.9,
-            scale_pos_weight=1,
+
+        self._trained_model = XGBClassifier(
+            learning_rate=0.02,
+            n_estimators=600,
             objective='multi:softprob',
-            reg_lambda=1,
-            reg_alpha=0
-        ).fit(
+            silent=True,
+            eval_set=[(test_rows, test_labels)],
+            early_stopping_rounds=5,
+        )
+
+        params = {
+            'min_child_weight': [1, 5, 10],
+            'gamma': [0.5, 1, 1.5, 2, 5],
+            'subsample': [0.6, 0.8, 1.0],
+            'colsample_bytree': [0.6, 0.8, 1.0],
+            'max_depth': [3, 4, 5]
+        }
+
+        # kfold_5 = KFold(n=len(train_rows), shuffle=True, n_folds=5)
+
+        randomized_search_cv = RandomizedSearchCV(
+            self._trained_model,
+            param_distributions=params,
+            random_state=42,
+            n_iter=200,
+            cv=3,
+            verbose=1,
+            n_jobs=4,
+            return_train_score=True
+        )
+        randomized_search_cv.fit(train_rows, train_labels)
+        self._trained_model = randomized_search_cv
+        # randomized_search_cv.cv_results_
+        """
+        self._trained_model.fit(
             train_rows,
             train_labels,
             eval_metric='auc'
@@ -39,6 +64,17 @@ class XGBoostClassifier:
         accuracy = round(good_prediction / len(test_predictions.tolist()), 5) * 100
         self.accuracy = round(accuracy, 3)
         return self.accuracy
+        """
+
+    def _calculate_accuracy(self, dataset: [TrainingScenario]):
+        good_prediction = 0
+        total_entry = len(dataset)
+        for training_scenario in dataset:
+            prediction = max(self.predict(training_scenario), key=lambda e: e[0])[0]
+            if training_scenario.training_label == prediction:
+                good_prediction += 1
+        return round(good_prediction/total_entry, 4)
+
         
     def _prepare_data_for_training(self, dataset: [TrainingScenario]):
         row_data = []
