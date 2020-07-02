00fc8057e9331d25290fc3a5aa0acd3456a088d6
Le palud Yves <ylepalud@oui.sncf>
2020-07-02 11:42:59 +0200

    Creating Training Producer

---
+++ rabbitMQ/TrainingProducer.py
@@ -0,0 +1,38 @@
+from rabbitMQ import RABBIT_MQ_HOST, RABBIT_MQ_PORT, RABBIT_MQ_SERVER, TRAINING_QUEUE, TRAINING_EXCHANGE
+from ml.Classifier import Classifier
+import pika
+
+
+class TrainingProducer:
+
+    def __init__(self):
+        self._connection = pika.BlockingConnection(
+            pika.connection.ConnectionParameters(
+                host=RABBIT_MQ_HOST,
+                port=RABBIT_MQ_PORT,
+                locale=RABBIT_MQ_SERVER
+            )
+        )
+        self._channel = self._connection.channel()
+
+        self._channel.exchange_declare(
+            exchange=TRAINING_EXCHANGE,
+            exchange_type="topic",
+            durable=True
+        )
+
+        self._channel.queue_declare(
+            queue=TRAINING_QUEUE,
+            durable=True
+        )
+
+    def send_trained_model(self, model: Classifier):
+        # TODO find a way to publish files
+        self._channel.basic_publish(
+            exchange=TRAINING_EXCHANGE,
+            routing_key=TRAINING_QUEUE,
+            body=""
+        )
+
+    def close(self):
+        self._connection.close()
--- rabbitMQ/__init__.py
+++ rabbitMQ/__init__.py
@@ -12,6 +12,8 @@ EGGPLANT_SUBMIT_PREDICTION_QUEUE = "eggplant-submit-prediction"
 EGGPLANT_SUBMIT_PREDICTION_EXCHANGE = "eggplant-submit-prediction"
 PREDICTION_QUEUE = "eggplant-prediction"
 PREDICTION_EXCHANGE = "eggplant-prediction"
+TRAINING_QUEUE = "eggplant-training"
+TRAINING_EXCHANGE = "eggplant-training"
 
 PREDICTION_ROUTING_KEY = "prediction"
 TRAIN_ROUTING_KEY = "train"
